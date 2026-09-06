00a8740cf89a2adcbe2b3cd4149cf7cffae5723b
ATUL BHASKARRAO PATHRIKAR <pathrikaratul7@gmail.com>
2026-09-07 01:27:39 +0530

    Add files via upload

---
+++ OpenAI_ChatGPT8777/Program.cs
@@ -0,0 +1,24 @@
+using OpenAI_ChatGPT8777.Interfaces;
+using OpenAI_ChatGPT8777.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient<OpenAITextService>();
+builder.Services.AddScoped<IOpenAITextService, OpenAITextService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
